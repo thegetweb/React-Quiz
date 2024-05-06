7fd39dfadeb3f0be41c9b56eb1572d70c094167f
thegetweb <thegetweb@gmail.com>
2024-05-06 09:30:41 -0700

    Some problem fix

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,7 +1,6 @@
 function StartScreen({ numQuestion, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz</h2>
       <h3>{numQuestion} question to test your React mastery</h3>
       <button
         className="btn btn-ui"
